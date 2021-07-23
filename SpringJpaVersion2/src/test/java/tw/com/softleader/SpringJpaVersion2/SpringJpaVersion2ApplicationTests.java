fa4bbc823652ec80b08c9aa8380012236243a7cf
Frank0321 <lefty21050@gmail.com>
2021-07-23 14:53:26 +0800

    新增 log 的測試

--- SpringJpaVersion2/src/test/java/tw/com/softleader/SpringJpaVersion2/SpringJpaVersion2ApplicationTests.java
+++ SpringJpaVersion2/src/test/java/tw/com/softleader/SpringJpaVersion2/SpringJpaVersion2ApplicationTests.java
@@ -1,13 +1,16 @@
 package tw.com.softleader.SpringJpaVersion2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Slf4j
 @SpringBootTest
 class SpringJpaVersion2ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("log info print");
+		log.debug("log debug print");
 	}
 
 }
