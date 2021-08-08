d4b377e817224964e458d16eb8748e518d7bd90e
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-08-08 09:24:36 +0800

    新增 @Tag 註解

--- SpringJpaVersion3/src/main/java/tw/com/softleader/SpringJpaVersion3/User/UserController.java
+++ SpringJpaVersion3/src/main/java/tw/com/softleader/SpringJpaVersion3/User/UserController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.Collection;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Tag(name = "User Controller", description = "user 的控制器")
 @RestController
 @RequestMapping("/version3/user")
 @RequiredArgsConstructor
