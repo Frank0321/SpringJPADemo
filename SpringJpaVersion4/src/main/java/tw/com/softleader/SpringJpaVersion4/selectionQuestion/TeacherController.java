e3317249a9c6b82b0029920fb47b014d074004a4
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-08-15 02:32:16 +0800

    新增 @Transactional 的測試

--- SpringJpaVersion4/src/main/java/tw/com/softleader/SpringJpaVersion4/selectionQuestion/TeacherController.java
+++ SpringJpaVersion4/src/main/java/tw/com/softleader/SpringJpaVersion4/selectionQuestion/TeacherController.java
@@ -2,6 +2,7 @@ package tw.com.softleader.SpringJpaVersion4.selectionQuestion;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +18,21 @@ public class TeacherController {
     final TeacherRepository teacherRepository;
     final StudentRepository studentRepository;
 
+    @Transactional
     @GetMapping("/saveTeacher")
     public String saveData1(){
         TeacherEntity teacherEntity = TeacherEntity.builder()
                 .id(1L)
                 .name("Rhys")
                 .teacherId("teacher1")
+                .students(List.of(StudentEntity.builder()
+                        .id(2L)
+                        .name("matt")
+                        .studentId("student1")
+                        .build()))
                 .build();
         log.info(teacherEntity.toString());
+
         teacherRepository.save(teacherEntity);
 
         return teacherEntity.toString();
